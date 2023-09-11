ca81d1cf332ecbc31fa171b3e05a8bf74a976cc5
Bjarne Koll <lynxplay101@gmail.com>
2023-09-11 13:01:45 +0200

    Ignore user fetch errors in envs without full user
    In systems without full user configuration, e.g. alpine, marauder should
    not fail to function, but rather simply fail if user information is used
    in a file path.
    
    This commit ignores user fetching errors and instead expects an error
    when executing the file template itself.

--- marauder-lib/pkg/utils/template.go
+++ marauder-lib/pkg/utils/template.go
@@ -26,11 +26,11 @@ func ExecuteStringTemplateToString[Data any](templateString string, data Data) (
 // EvaluateFilePathTemplate evaluates a file path template for a cobra command.
 func EvaluateFilePathTemplate(filePathTemplate string) (string, error) {
 	// fetch user and expand tls path
-	userAccount, err := user.Current()
-	if err != nil {
-		return "", fmt.Errorf("failed to fetch current user: %w", err)
-	}
-	filePathTemplate, err = ExecuteStringTemplateToString(filePathTemplate, struct{ User *user.User }{User: userAccount})
+	// If we cannot fully fetch the user, the env does not support user fetching. Proceed anyway, they could not use variables
+	// in their path. They get a nil user, if they access it the templating will fail.
+	userAccount, _ := user.Current()
+
+	filePathTemplate, err := ExecuteStringTemplateToString(filePathTemplate, struct{ User *user.User }{User: userAccount})
 	if err != nil {
 		return "", fmt.Errorf("failed to evaluate filePathTemplate: %w", err)
 	}
