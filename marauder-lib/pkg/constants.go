c42b110250174e102d11445335fab9b5ae3d5728
Bjarne Koll <lynxplay101@gmail.com>
2024-03-15 16:04:37 +0100

    Allow overriding build information via env vars
    Consumers of the marauder clients that build artefacts might be
    interested in bypassing marauders build information fetching based on
    the git repository.
    
    This is specifically interesting in contexts like CI servers that might
    not fully fetch a repository and hence marauder might not be picking up
    the correct values.

--- marauder-lib/pkg/constants.go
+++ marauder-lib/pkg/constants.go
@@ -14,3 +14,11 @@ const TLSPoolDir = "pool"
 
 // The FileParentDirectoryInArtefact holds the prefix under which all files are stored in the artefact tarball.
 const FileParentDirectoryInArtefact = "files/"
+
+// The MarauderEnvironmentBranchOverride constant defines the environment name that may be defined when calling the
+// marauder client to overwrite the git branch detection.
+const MarauderEnvironmentBranchOverride = "MARAUDER_BRANCH_OVERRIDE"
+
+// The MarauderEnvironmentBuildSpecificVersionOverride constant defines the environment name that may be defined when calling the
+// marauder client to overwrite the build-specific version.
+const MarauderEnvironmentBuildSpecificVersionOverride = "MARAUDER_BUILD_SPECIFIC_VERSION_OVERRIDE"
