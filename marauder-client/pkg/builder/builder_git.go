f9a89366a92631064ddfe4fb0fb0e0f1ea57d30a
Bjarne Koll <lynxplay101@gmail.com>
2024-03-15 16:04:37 +0100

    Allow overriding build information via env vars
    Consumers of the marauder clients that build artefacts might be
    interested in bypassing marauders build information fetching based on
    the git repository.
    
    This is specifically interesting in contexts like CI servers that might
    not fully fetch a repository and hence marauder might not be picking up
    the correct values.

--- marauder-client/pkg/builder/builder_git.go
+++ marauder-client/pkg/builder/builder_git.go
@@ -2,8 +2,11 @@ package builder
 
 import (
 	"fmt"
+	"os"
 	"strings"
 	"time"
+
+	"gitea.knockturnmc.com/marauder/lib/pkg"
 
 	"gitea.knockturnmc.com/marauder/lib/pkg/models/filemodel"
 	"github.com/go-git/go-git/v5"
@@ -33,15 +36,29 @@ func FetchBuildInformation(path string) (filemodel.BuildInformation, error) {
 		return filemodel.BuildInformation{}, fmt.Errorf("failed to locate commit information from head hash: %w", err)
 	}
 
+	// Allow overriding the branch name fetched if e.g. this isn't run in a fully checked out repo.
+	branchFromGitRef := strings.Replace(head.Name().String(), "refs/head/", "", 1)
+	branchFromEnv, found := os.LookupEnv(pkg.MarauderEnvironmentBranchOverride)
+	if found {
+		branchFromGitRef = branchFromEnv
+	}
+
+	// Allow overriding the build specific version fetched if e.g. this isn't run in a fully checked out repo.
+	buildSpecificVersionFromGit := "g" + commit.Hash.String()[:7]
+	buildSpecificVersionFromEnv, found := os.LookupEnv(pkg.MarauderEnvironmentBuildSpecificVersionOverride)
+	if found {
+		buildSpecificVersionFromGit = buildSpecificVersionFromEnv
+	}
+
 	information := filemodel.BuildInformation{
 		Repository:           remote.Config().URLs[0],
-		Branch:               strings.Replace(head.Name().String(), "refs/head/", "", 1),
+		Branch:               branchFromGitRef,
 		CommitUser:           commit.Author.Name,
 		CommitEmail:          commit.Author.Email,
 		CommitHash:           commit.Hash.String(),
 		CommitMessage:        strings.TrimRight(commit.Message, "\n"),
 		Timestamp:            time.Now(),
-		BuildSpecificVersion: "g" + commit.Hash.String()[:7],
+		BuildSpecificVersion: buildSpecificVersionFromGit,
 	}
 
 	return information, nil
